78cdb4a7e2257851203f1f920ca3079b648f201e
pist <npistiolis@gmail.com>
2015-04-10 21:39:43 +0300

    Commit message

--- plot3.R
+++ plot3.R
@@ -8,6 +8,6 @@ data_y3<-as.numeric(levels(data2$V9))[data2$V9]
 plot(data_x,data_y1,"l",xlab="",ylab="Energy Sub metering")
 lines(data_x,data_y2,col="red")
 lines(data_x,data_y3,col="blue")
-legend("topright",col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),y.intersp=0.5,,lty=1,cex=0.60)
+legend("topright",col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),y.intersp=0.5,lty=1,cex=0.60)
 dev.copy(png, file="plot3.png",width=480,height=480)
 dev.off() 
